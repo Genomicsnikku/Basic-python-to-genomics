4c855db69f52c4090d1ecd3e41db0570df067d15
Nikku | Bioinformatics | Python <n3016507@gmail.com>
2026-08-27 17:20:19 +0530

    Create 02_datatypes_variables.py

---
+++ 02_datatypes_variables.py
@@ -0,0 +1,45 @@
+# Chapter 2 - Variables and Data Types
+
+# Variable is a container
+a = 30
+b = "Nikki"
+c = 71.22
+print(a)
+print(b)
+print(c)
+
+# Data Types
+x = 71
+y = 88.44
+name = "Nikki"
+print(type(x))
+print(type(y))
+print(type(name))
+
+# Variable Rules
+my_name = "Nikki"
+one8 = 18
+seven_ = 7
+_seven = 7
+
+# Operators
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+
+a = 5
+a += 2
+print(a)
+
+print(10 == 3)
+print(True and False)
+
+# Typecasting
+print(str(31))
+print(int("32"))
+print(float(32))
+
+# input() function - always returns string
+user_name = input("Enter name: ")
+print(user_name)
