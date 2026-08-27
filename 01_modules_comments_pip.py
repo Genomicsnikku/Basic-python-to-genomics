9ccbf768e8df3eb82facd7151e9b9df8ea2cdc74
Nikku | Bioinformatics | Python <n3016507@gmail.com>
2026-08-27 17:00:57 +0530

    Chapter 1: Modules, Comments and pip

---
+++ 01_modules_comments_pip.py
@@ -0,0 +1,15 @@
+# 1. Module Code Example
+import math
+print("Value of Pi using module:", math.pi)
+
+# 2. Comment Code Example
+# Yeh ek single line comment hai
+print("Hello World!")
+
+"""
+Yeh ek multi-line comment hai.
+Python isko ignore karta hai.
+"""
+
+# 3. Pip Command (Yeh terminal mein likhi jaati hai)
+# pip install flask
